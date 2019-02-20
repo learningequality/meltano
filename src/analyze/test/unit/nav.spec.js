b1a6af1aef81c25044b44732735248ed3ea881ea
Jacob Schatz <jschatz@gitlab.com>
2019-02-20 11:09:34 -0500

    add initial tests

---
+++ src/analyze/test/unit/nav.spec.js
@@ -0,0 +1,11 @@
+import { shallowMount } from '@vue/test-utils';
+import MainNav from '@/components/MainNav.vue';
+
+describe('MainNav.vue', () => {
+  it('renders props.msg when passed', () => {
+    const msg = 'new message';
+    const wrapper = shallowMount(MainNav);
+    // expect(wrapper.text()).toMatch(msg);
+    console.log(wrapper);
+  });
+});
