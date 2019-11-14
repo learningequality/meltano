58e0828f5000b587d95299a89237ca2c0afa72cb
derek-knox <dknox@gitlab.com>
2019-11-14 13:50:57 -0800

    added notes for context

--- src/webapp/src/redash/meltano/RedashQueryContainer.js
+++ src/webapp/src/redash/meltano/RedashQueryContainer.js
@@ -11,11 +11,29 @@ export default class ReactTest extends React.Component {
       <div>
         <h1>Hello, World w/Angular + React in Vue</h1>
 
+        <hr />
+
         <h2>React usage:</h2>
         <Timer from={time} />
 
+        <hr />
+
         <h2>Angular usage:</h2>
         <rd-timer from={`"${time}"`} />
+
+        <hr />
+
+        <h2>Angular+React Complex usage:</h2>
+        <p>Stopping point = dual router crossroads. Options for next steps:</p>
+        <div className="content">
+          <ul>
+            <li>Try having dual routers (Vue and now Angular). Scares me.</li>
+            <li>
+              Extracting Angular markup + corresponding controller subsets and
+              converting to React (redash contribute back) or Vue
+            </li>
+          </ul>
+        </div>
       </div>
     )
   }
