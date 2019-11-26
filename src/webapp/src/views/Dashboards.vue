fe3f507149ac1c1ddbcccb066a9f7c613cc7b4b1
derek-knox <dknox@gitlab.com>
2019-11-25 16:31:08 -0800

    initialize by default due to dashboards dropdown now holding the CTAs (no empty landing page now)

--- src/webapp/src/views/Dashboards.vue
+++ src/webapp/src/views/Dashboards.vue
@@ -14,7 +14,7 @@ export default {
   data() {
     return {
       isActiveDashboardLoading: false,
-      isInitializing: false,
+      isInitializing: true,
       isNewDashboardModalOpen: false
     }
   },
