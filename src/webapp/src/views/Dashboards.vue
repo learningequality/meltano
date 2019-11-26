6b22939359cd4f0d85026ec2f184e41f5b495beb
derek-knox <dknox@gitlab.com>
2019-11-25 16:18:24 -0800

    updated dashboards to be multiline columns

--- src/webapp/src/views/Dashboards.vue
+++ src/webapp/src/views/Dashboards.vue
@@ -14,7 +14,6 @@ export default {
   data() {
     return {
       isActiveDashboardLoading: false,
-      isInitializing: false,
       isNewDashboardModalOpen: false
     }
   },
@@ -40,12 +39,6 @@ export default {
   beforeDestroy() {
     this.$store.dispatch('dashboards/resetActiveDashboard')
     this.$store.dispatch('dashboards/resetActiveDashboardReports')
-  },
-  created() {
-    this.isInitializing = true
-    this.initialize(this.$route.params.slug).then(() => {
-      this.isInitializing = false
-    })
   },
   methods: {
     ...mapActions('dashboards', [
@@ -86,96 +79,63 @@ export default {
             <h3 v-if="activeDashboard.description" class="subtitle">
               {{ activeDashboard.description }}
             </h3>
+            <progress
+              v-if="isActiveDashboardLoading"
+              class="progress is-small is-info"
+            >
+            </progress>
           </div>
         </div>
 
-        <div class="columns">
-          <div class="column">
-            <template v-if="activeDashboard.name">
-              <progress
-                v-if="isActiveDashboardLoading"
-                class="progress is-small is-info"
-              >
-              </progress>
-              <template v-else>
-                <template v-if="activeDashboardReports.length > 0">
-                  <div
-                    v-for="report in activeDashboardReports"
-                    :key="report.id"
-                    class='box'
-                  >
-                    <div class="level">
-                      <div class="level-left">
-                        <div class="level-item">
-                          <div class="content">
-                            <h5 class="has-text-centered">
-                              {{ report.name }}
-                            </h5>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="level-right">
-                        <div class="level-item">
-                          <div class="buttons">
-                            <a
-                              class="button is-small"
-                              @click="goToReport(report)"
-                              >Edit</a
-                            >
-                            <a
-                              class="button is-small"
-                              @click="goToDesign(report)"
-                              >Explore</a
-                            >
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <chart
-                      :chart-type="report.chartType"
-                      :results="report.queryResults"
-                      :result-aggregates="report.queryResultAggregates"
-                    ></chart>
+        <div class="columns is-multiline">
+          <div class="column is-half"
+            v-for="report in activeDashboardReports"
+            :key="report.id"
+          >
+          <div class='box'>
+            <div class="level">
+              <div class="level-left">
+                <div class="level-item">
+                  <div class="content">
+                    <h5 class="has-text-centered">
+                      {{ report.name }}
+                    </h5>
                   </div>
-                </template>
-                <div v-else class="content">
-                  <p>There are no reports added to this dashboard yet.</p>
-                  <router-link
-                    class="button is-interactive-primary is-outlined"
-                    :to="{ name: 'model' }"
-                    >Analyze Some Data First</router-link
-                  >
                 </div>
-              </template>
-            </template>
-
-            <template v-else>
-              <div class="column content">
-                <p>
-                  Select a dashboard or click "New" to the left and add
-                  reports to it to view them here.
-                </p>
-                <p>You can add a report by:</p>
-                <ul>
-                  <li>
-                    In Analyze, clicking "Add to Dashboard" after creating a
-                    report
-                  </li>
-                  <li>
-                    Clicking the "Add Report" button to an existing dashboard
-                    (coming soon)
-                  </li>
-                </ul>
               </div>
-            </template>
-
-            <NewDashboardModal
-              v-if="isNewDashboardModalOpen"
-              @close="toggleNewDashboardModal"
-            />
+              <div class="level-right">
+                <div class="level-item">
+                  <div class="buttons">
+                    <a
+                      class="button is-small"
+                      @click="goToReport(report)"
+                      >Edit</a
+                    >
+                    <a
+                      class="button is-small"
+                      @click="goToDesign(report)"
+                      >Explore</a
+                    >
+                  </div>
+                </div>
+              </div>
+            </div>
+            <chart
+              :chart-type="report.chartType"
+              :results="report.queryResults"
+              :result-aggregates="report.queryResultAggregates"
+            ></chart>
           </div>
+          </div>
+
         </div>
       </section>
+
+      <NewDashboardModal
+        v-if="isNewDashboardModalOpen"
+        @close="toggleNewDashboardModal"
+      />
+
     </div>
   </router-view-layout>
 </template>
