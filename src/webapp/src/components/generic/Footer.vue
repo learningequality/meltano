a899f1af17acceffea7db9db6597d590dfaca699
derek-knox <dknox@gitlab.com>
2019-12-06 17:40:58 -0800

    updated footer links for tos and privacy policy

--- src/webapp/src/components/generic/Footer.vue
+++ src/webapp/src/components/generic/Footer.vue
@@ -7,10 +7,16 @@ export default {
 <template>
   <footer class="footer">
     <div class="buttons are-small has-addons is-right">
-      <a class="button is-text" href="https://www.meltano.com" target="_blank"
+      <a
+        class="button is-text"
+        href="https://meltano.com/docs/security-and-privacy.html#terms-of-service"
+        target="_blank"
         >Terms of Service</a
       >
-      <a class="button is-text" href="https://www.meltano.com" target="_blank"
+      <a
+        class="button is-text"
+        href="https://meltano.com/docs/security-and-privacy.html#privacy-policy"
+        target="_blank"
         >Privacy Policy</a
       >
     </div>
