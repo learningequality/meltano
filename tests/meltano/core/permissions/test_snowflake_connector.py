890352859051af32e11f6aa8100fc47469cf2854
Yannis Roussos <iroussos@gitlab.com>
2020-01-27 17:31:56 +0200

    Add initial example of how to mock SnowflakeConnector connections to Snowflake server

--- tests/meltano/core/permissions/test_snowflake_connector.py
+++ tests/meltano/core/permissions/test_snowflake_connector.py
@@ -1,6 +1,23 @@
 import pytest
 
+from unittest.mock import patch
+
 from meltano.core.permissions.utils.snowflake_connector import SnowflakeConnector
+
+@pytest.fixture(scope="class")
+def test_schemas():
+    schemas = [
+        "test_db.test_schema1",
+        "test_db.test_schema2",
+        "test_db.test_schema3",
+        "test_db.test_schema4",
+        "test_db.test2_schema1",
+        "test_db.test2_schema2",
+        "test_db.test2_schema3",
+        "test_db.test2_schema4",
+    ]
+
+    return schemas
 
 
 class TestSnowflakeConnector:
@@ -15,3 +32,17 @@ class TestSnowflakeConnector:
         assert SnowflakeConnector.snowflaky(db2) == "1234raw.schema.table"
         assert SnowflakeConnector.snowflaky(db3) == '"123-with-quotes".schema.table'
         assert SnowflakeConnector.snowflaky(db4) == '"1_db-9-RANDOM".schema.table'
+
+    def test_mocked_schemas(self, test_schemas):
+
+        with patch.object(
+            SnowflakeConnector, "show_schemas", return_value=test_schemas
+        ), patch.object(
+            SnowflakeConnector, "__init__", return_value=None
+        ):
+            conn = SnowflakeConnector()
+
+            test_schema = 'test_db.test_*'
+            schemas = conn.full_schema_list(test_schema)
+
+            assert len(schemas) == 4
